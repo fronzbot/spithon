150313ab6f627410f8946072e996aa63a2ac6d39
Kevin Fronczak <kfronczak@gmail.com>
2023-06-15 21:31:09 -0400

    Fix classifier formatting for pypi upload

--- spithon/__init__.py
+++ spithon/__init__.py
@@ -2,7 +2,7 @@
 
 YEAR = 2023
 MONTH = 6
-PATCH = 0
+PATCH = 1
 
 __version__ = f"{YEAR}.{MONTH}.{PATCH}"
 
@@ -12,15 +12,15 @@ PROJECT_EMAIL = "kfronczak@gmail.com"
 PROJECT_URL = "https://github.com/fronzbot/spithon"
 PROJECT_LICENSE = "A CLI for SPI/GPIO manipulation on a Raspberry Pi."
 PROJECT_DESCRIPTION = "A Raspberry Pi CLI tool for SPI and GPIO communication."
-PROJECT_CLASSIFIERS = (
-    "Intended Audience :: Developers"
-    "Intended Audience :: Science/Research"
-    "Licence :: OSI Approved :: Apache Software License"
-    "Programming Language :: Python :: 3.8"
-    "Programming Language :: Python :: 3.9"
-    "Programming Language :: Python :: 3.10"
-    "Programming Language :: Python :: 3.11"
-    "Topic :: Scientific/Engineering :: Interface Enginer/Protocol Translator"
-    "Topic :: Software Development :: Libraries :: Python Modules"
-    "Topic :: Terminals :: Serial"
-)
+PROJECT_CLASSIFIERS = [
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Licence :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Topic :: Scientific/Engineering :: Interface Enginer/Protocol Translator",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Terminals :: Serial",
+]
