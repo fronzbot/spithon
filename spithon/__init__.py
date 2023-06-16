d54926fb74e0334e86214360e02ea2a699f40883
Kevin Fronczak <kfronczak@gmail.com>
2023-06-15 21:43:59 -0400

    Change versioning again and another dumb pypi fix

--- spithon/__init__.py
+++ spithon/__init__.py
@@ -1,7 +1,7 @@
 """Init file for CLI."""
 
-MAJOR = 1
-MINOR = 0
+MAJOR = 0
+MINOR = 1
 PATCH = 0
 
 __version__ = f"{MAJOR}.{MINOR}.{PATCH}"
@@ -13,14 +13,9 @@ PROJECT_URL = "https://github.com/fronzbot/spithon"
 PROJECT_LICENSE = "A CLI for SPI/GPIO manipulation on a Raspberry Pi."
 PROJECT_DESCRIPTION = "A Raspberry Pi CLI tool for SPI and GPIO communication."
 PROJECT_CLASSIFIERS = [
-    "Intended Audience :: Developers",
-    "Intended Audience :: Science/Research",
     "License :: OSI Approved :: Apache Software License",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
-    "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: Terminals :: Serial",
 ]
